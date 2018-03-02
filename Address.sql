2c2594e38a7283b200613d5e24daab7d14d7293a
rishafc <rishronit@gmail.com>
2018-03-02 10:47:14 -0500

    Update Address.sql

--- Address.sql
+++ Address.sql
@@ -2,8 +2,12 @@
 -- version 4.7.7
 -- https://www.phpmyadmin.net/
 --
+-- Host: localhost-- phpMyAdmin SQL Dump
+-- version 4.7.7
+-- https://www.phpmyadmin.net/
+--
 -- Host: localhost
--- Generation Time: Mar 02, 2018 at 04:39 PM
+-- Generation Time: Mar 02, 2018 at 04:45 PM
 -- Server version: 10.1.30-MariaDB
 -- PHP Version: 7.2.1
 
@@ -32,9 +36,48 @@ CREATE TABLE `Address` (
   `State` char(15) NOT NULL,
   `City` char(15) NOT NULL,
   `Street` text NOT NULL,
-  `Zip` tinyint(10) NOT NULL
+  `Zip` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `Address`
+--
+
+INSERT INTO `Address` (`State`, `City`, `Street`, `Zip`) VALUES
+('PA', 'state college', 'pollock rd', 16801),
+('New York', 'New York', 'manhattan street', 11210);
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+-- Generation Time: Mar 02, 2018 at 04:39 PM
+-- Server version: 10.1.30-MariaDB
+-- PHP Version: 7.2.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dCoders`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Address`
+--
+
+
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
